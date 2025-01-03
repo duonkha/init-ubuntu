a004d1ddbcaf99530406a5f4ad81d5f68e12fbe1
Khanh Duong <30329095+duonkha@users.noreply.github.com>
2025-01-03 12:27:08 -0800

    Add gcc

--- init_intall.sh
+++ init_intall.sh
@@ -6,7 +6,7 @@ sudo apt update && sudo apt upgrade -y
 
 # ==================== Install Prerequisites ====================
 echo "Installing prerequisites..."
-sudo apt install -y software-properties-common curl git
+sudo apt install -y software-properties-common curl git gcc
 
 # ==================== Fish Shell ====================
 if ! command -v fish &>/dev/null; then
